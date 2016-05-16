06066efa3d74e8bf30e16a465ec0da4cfc77aa22
Gustavo Del Negro <gustavodelnegro@gmail.com>
2016-05-15 23:47:52 -0300

    app runing

--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.conf import settings
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    # return render_to_response('portfolio/index.html', locals())
+    return render(request, 'portfolio/index.html', {"appActive": "common", "pageActive": "dashboard-main", "sidebarActive": "dashboard", 'settings': settings})
