30ef06b00ab5c8280bc093e423322a69c1a01373
Gustavo Del Negro <gustavodelnegro@gmail.com>
2016-05-16 12:29:18 -0300

    translation

--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
-
+from portfolio.models import *
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     # return render_to_response('portfolio/index.html', locals())
-    return render(request, 'portfolio/index.html', {"appActive": "common", "pageActive": "dashboard-main", "sidebarActive": "dashboard", 'settings': settings})
+    return render(request, 'portfolio/index.html', {
+        "appActive": "portfolio",
+        'settings': settings,
+        'projects': Projects.objects.all(),
+        'projects_types': ProjectTypes.objects.all()
+        })
