959114f51ce97a6098a1b6c263a142f4dae57405
Gustavo Del Negro <gustavodelnegro@gmail.com>
2016-05-16 12:12:23 -0300

    translation

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -130,7 +130,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = [
     STATIC_ROOT,
-    '/static'
+    '/static',
+    '/portfolio/static'
 ]
 
 #LOCALE
@@ -148,4 +149,3 @@ LANGUAGES = (
 )
 
 MODELTRANSLATION_FALLBACK_LANGUAGES = {'default': ('en',)}
-
