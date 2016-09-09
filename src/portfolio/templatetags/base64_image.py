2ce2fa4800eb4ab7beed4bd61ef1b59457b7a71a
gdelnegro <gustavodelnegro@gmail.com>
2016-09-09 09:47:57 -0300

    Updated base_64 templatetag, now getting the image data from the model; Added projects to index

--- src/portfolio/templatetags/base64_image.py
+++ src/portfolio/templatetags/base64_image.py
@@ -1,21 +1,27 @@
 from django import template
+from django.conf import settings
+from portfolio.models import Image
 register = template.Library()
 
 
 @register.filter('base64_image')
-def base64_image(mime, encoded_string):
+def base64_image(image_id):
     """
-    Template tag made as a helper for displaying
-    base64 images.
+    Template tag made as a helper for displaying base64 images.
+    Id a image with the given id doesn't exists, the placeholder image wil be returned
     Parameters:
-        mime: image mime type
-        enconded_string:  image base64 string
+        image_id: the id of the image you want to display
     Usage:
         {% load base64_image %}
         [...]
-        <img src="{{ user.userextend.photo.mimetype|base64_image:user.userextend.photo.image_string }}">
+        <img src="{{ projects.images.all.first.id|base64_image }}">
         [...]
     Return:
         string, data:mime;base64,encoded_string
     """
-    return "data:%s;base64,%s" % (mime, encoded_string)
+    try:
+        img = Image.objects.get(pk=image_id)
+    except Image.DoesNotExist:
+        return settings.PLACEHOLDER_B64_STRING
+    else:
+        return "data:%s;base64,%s" % (img.mimetype, img.image_string)
