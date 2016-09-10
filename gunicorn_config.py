71f94aa5d94b69290aa8443238a5e0c0e3b2686e
gdelnegro <gustavodelnegro@gmail.com>
2016-09-10 07:59:21 -0300

    added basic nginx and gunicorn configuration

---
+++ gunicorn_config.py
@@ -0,0 +1,5 @@
+command = '/home/gdelnegro/projects/gdelnegro/portfolio/bin/gunicorn'
+pythonpath = '/home/gdelnegro/projects/gdelnegro/portfolio/src'
+bind = '0.0.0.0:8001'
+workers = 2
+user = 'nobody'
---
+++ gunicorn_start.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+NAME="portfolio"                                  # Name of the application
+DJANGODIR=/home/gdelnegro/projects/gdelnegro/portfolio/src             # Django project directory
+SOCKFILE=/home/gdelnegro/projects/gdelnegro/portfolio/src/portfolio.sock  # we will communicte using this unix socket
+USER=gdelnegro                                        # the user to run as
+GROUP=gdelnegro                                    # the group to run as
+NUM_WORKERS=1                                     # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=portfolio.settings             # which settings file should Django use
+DJANGO_WSGI_MODULE=portfolio.wsgi                     # WSGI module name
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source ../bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec ../bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-file=-
