2ee5ce142846f7013389cc6b7704365917b30d46
gdelnegro <gustavodelnegro@gmail.com>
2016-09-13 10:20:20 -0300

    Updated static files; Updated admin

--- src/portfolio/admin.py
+++ src/portfolio/admin.py
@@ -12,6 +12,7 @@ class CustomModelAdminMixin(object):
     def __init__(self, model, admin_site):
         self.list_display = [field.name for field in model._meta.fields if field.name != "id"]
         super(CustomModelAdminMixin, self).__init__(model, admin_site)
+        self.filter_horizontal = [field.name for field in model._meta.get_fields() if field.many_to_many]
 
 
 class TechnologyAdmin(CustomModelAdminMixin, TabbedTranslationAdmin):
