424f6cc846c6d4bd78d4157f3163d0af7898a7c5
gdelnegro <gustavodelnegro@gmail.com>
2016-09-16 10:56:56 -0300

    added contact_email template; Added email settings

--- src/portfolio/settings/development.py
+++ src/portfolio/settings/development.py
@@ -36,3 +36,9 @@ DATABASES = {
 }
 
 ENV = "DEV"
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
--- src/portfolio/settings/production.py
+++ src/portfolio/settings/production.py
@@ -36,3 +36,10 @@ DATABASES = {
 
 STATIC_ROOT = "/srv/portfolio/src/static/"
 ENV = "PROD"
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
